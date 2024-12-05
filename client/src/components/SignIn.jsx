3c8f18f7643fede85fbdf5c6dd0a4f2250febda7
Nouran246 <111746732+Nouran246@users.noreply.github.com>
2024-12-06 00:14:35 +0200

    log in update

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 function SignIn(){
-    return(
-        <div>Sign In</div>
-    )
+   return <div className="form-container">
+<h2>Login</h2>
+<form >
+    <div className='form-control'>
+    <input type="text" placeholder='Enter your email'/>
+
+    </div>
+    <div className='form-control'>
+    <input type="password" placeholder='Enter your password'/>
+
+    </div>
+</form>
+   </div>
 }
 export default SignIn
