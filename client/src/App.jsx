76cc9200aa596b6ec52d6325b3fd64a57adf6760
Nouran246 <111746732+Nouran246@users.noreply.github.com>
2024-12-06 00:32:50 +0200

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import SignIn from "./components/SignIn";
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
  function App(){
   return <signIn />
  }
