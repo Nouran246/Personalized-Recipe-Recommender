f1f839123d13c67d107400e5173282fbe530eeea
Nouran246 <111746732+Nouran246@users.noreply.github.com>
2024-12-17 16:45:24 +0200

    Revert "all done"
    This reverts commit 557771d3920e7ac66c6b7c875936eda26d6a0e53.

--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 
 const Card = ({ data }) => {
-  const { id, title, description, image } = data;
+  const { title, description, image } = data;
 
   return (
-    <>
-    
     <div className="card">
       <img src={image} alt={title} className="card-image" />
       <div className="card-content">
         <h3>{title}</h3>
         <p>{description}</p>
-        <button  className="search-button">Click here</button>
       </div>
     </div>
-    </>
   );
 };
 
 export default Card;
-
--- client/src/Demo.jsx
+++ client/src/Demo.jsx
@@ -1,34 +1,28 @@
-import React from "react";
+import React from 'react';
 
-const Demo = () => {
-  return (
-    <>
-      <header>
-        <nav class="navbar">
-          <div class="navbar-left">
-            <a href="#home" className="navbar-link">
-              Home
-            </a>
-            <a href="#about" className="navbar-link">
-              About
-            </a>
+const Demo = ()=>{
+return(
+        <>
+<header>
+<nav class="navbar">
+    <div class="navbar-left">
+    <a href="#home" class="navbar-link">Home</a>
+    <a href="#about" class="navbar-link">About</a>
+    <a href="#about" class="navbar-link">Pricing</a>
+    <a href="#about" class="navbar-link">Dishes</a>
+        
+    </div>
+    <div class="navbar-right">
+    <a href="#login" class="navbar-link">Login</a>
+    <a href="#signup" class="signup_navbar-link">Signup</a>
+        
+    </div>
+</nav>
+    </header>
             
-            <a href="#dishes" className="navbar-link">
-              Dishes
-            </a>
-          </div>
-          <div class="navbar-right">
-            <a href="#login" class="navbar-link">
-              Login
-            </a>
-            <a href="#signup" class="signup_navbar-link">
-              Signup
-            </a>
-          </div>
-        </nav>
-      </header>
-    </>
-  );
-};
+        
+        </>
+    )
+}
 
 export default Demo;
